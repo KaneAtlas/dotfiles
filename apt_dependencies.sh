8c47508a79afd68fc3701dfc1f2e6cd7600ec791
KaneAtlas <kisbyalexander@gmail.com>
2022-05-25 18:23:15 +0100

    fixed pyenv and added chrome

--- apt_dependencies.sh
+++ apt_dependencies.sh
@@ -2,7 +2,7 @@
 #installs all basic apt dependencies
 
 #terminal stuff
-sudo apt install vim neovim kitty fish snap curl fortune
+sudo apt install vim neovim kitty fish snap curl wget dpkg fortune
 #git
 sudo apt install git-all
 #pyenv dependencies
@@ -10,4 +10,9 @@ sudo apt update -y
 sudo apt install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev\
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl\
+gcc
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev pip3
+
 
--- other_dependencies.sh
+++ other_dependencies.sh
@@ -3,6 +3,8 @@
 
 #slack
 sudo snap install slack --classic
+#chrome
+source get_chrome.sh
 #vimplug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
